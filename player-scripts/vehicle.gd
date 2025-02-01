03d6e5b70448feffb636c2a3eeb3820aeff0fd38
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2025-02-01 15:05:23 -0500

    improve car controller

--- player-scripts/vehicle.gd
+++ player-scripts/vehicle.gd
@@ -1,41 +1,51 @@
 extends CharacterBody3D
 class_name CustomCarController
 
-@export var s: Curve
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
-@onready var VEHICLE_MODEL: Node3D = $"vehicle-suv3"
+# You're expected to provide a ray pointing in the 'forward' direction
+# for your vehicle
+# ray_x_basis will be used to track the direction it's pointing
+# So that we don't have to access the ray every time we want this info
 @onready var forward_ray: RayCast3D = $RayCast3D
+#var ray_x_basis
+
+# This needs to be renamed, but it's the curve at which we're rotating our vehicle
+@export var s: Curve
+
+# OUR VEHICLE CAN JUMP????
+@export var JUMP_VELOCITY = 4.5
+
+
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
+# vehicle configuration that should be put into a resource honestly..
+@export var SPEED = 5.0
 @onready var CURRENT_MPH: float = 3.0
+@onready var TRACTION: float = 0.1
+
+# TODO: acceleration should be a curve
 @onready var ACCELERATION: float = 0.2
 
-var facing_dir
+@onready var VEHICLE_MODEL: Node3D = $"vehicle-suv3"
 
-# we just want the rotation to know where we're facing..
-func set_facing_dir():
-	#print(forward_ray.get_global_transform().basis)
-	facing_dir = forward_ray.transform.basis.x
-	
 
-func _ready():
-	set_facing_dir()
-	pass
+func get_ray_x_basis(ray: RayCast3D):
+	# ray could be null
+	if ray:
+		return ray.transform.basis.x
 
 
 func rotate_model_to_direction(delta, dir: Vector3):
-	var rotation_to_do = lerp(0.0, - dir.x, s.sample(delta))
+	#var rotation_to_do = lerp(0.0, - dir.x, s.sample(delta))
+	var rotation_to_do = move_toward(0.0, - dir.x, 0.1 * s.sample(delta))
 	
 	VEHICLE_MODEL.rotate_y(rotation_to_do)
 	forward_ray.rotate_object_local(Vector3.FORWARD, rotation_to_do)
 	collision_shape_3d.rotate_y(rotation_to_do)
 	
-	set_facing_dir()
 	pass
 
-func get_current_speed() -> Vector3:
-	return velocity
+func get_current_speed():
+	return CURRENT_MPH
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -45,23 +55,24 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-
-	
 	
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	if Input.is_action_pressed("ui_up"):
+		CURRENT_MPH = move_toward(CURRENT_MPH, CURRENT_MPH + 1, ACCELERATION)
+	elif Input.is_action_pressed("ui_down"):
+		CURRENT_MPH = move_toward(CURRENT_MPH, 0, ACCELERATION)
+	
 	if direction:
 		rotate_model_to_direction(delta, direction)
 
-	if facing_dir:
+	var ray_x_basis = get_ray_x_basis(forward_ray)
+	if ray_x_basis:
 		# https://docs.godotengine.org/en/latest/tutorials/3d/using_transforms.html
-		velocity.z = - facing_dir.x * CURRENT_MPH
-		velocity.x = facing_dir.z * CURRENT_MPH
-
-	#else:
-		#velocity.x = move_toward(velocity.x, CURRENT_MPH, SPEED)
-		#velocity.z = move_toward(velocity.z, CURRENT_MPH, SPEED)
+		# NO, the flipped X / Z are not incorrect
+		# SWITCHING these or changing this without understanding the consequences... is DIRE
+		velocity.z = move_toward(velocity.z, - ray_x_basis.x * CURRENT_MPH, TRACTION)
+		velocity.x = ray_x_basis.z * CURRENT_MPH
 
 	move_and_slide()
